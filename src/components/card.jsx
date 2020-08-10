a8de1a4aaf8bb1dde86469223b53fc73fc64467d
Sivarajan <citysiva180@gmail.com>
2020-08-10 09:54:14 +0530

    intialCommit

---
+++ src/components/card.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const cards = () => {
+  return <h1>card</h1>;
+};
+
+export default cards;
---
+++ src/components/cardComponents.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Cardcomponent = () => {
+  return <h1>CardComponent</h1>;
+};
+
+export default Cardcomponent;

196620d172f494b1356982b6d62d6262033c4389
Sivarajan <citysiva180@gmail.com>
2020-08-11 18:04:22 +0530

    dynamically_added_cards

--- src/components/cardComponents.jsx
+++ src/components/cardComponents.jsx
@@ -1,11 +1,35 @@
 import React from "react";
+import "../components/card.css";
+import sivapic from "../images/IMG_5541.jpg";
+import rampic from "../images/IMG_4822.jpg";
+import dkpic from "../images/IMG_3367.jpg";
+import satzpic from "../images/IMG_E3874.jpg";
+import "bootstrap/dist/css/bootstrap.css";
 
 const Cardcomponent = () => {
-  return (
-    <div className="Cards">
-      <h1>Hello</h1>
-    </div>
-  );
+  const cardInfo = [
+    { image: sivapic, title: "Siva", text: "Fullstack Developer" },
+    { image: rampic, title: "Ram", text: "Cyber security Engineer" },
+    { image: satzpic, title: "RockySatz", text: "Python Engineer" },
+    { image: dkpic, title: "Dk", text: "Fullstack Developer" },
+  ];
+
+  const renderCard = (card, index) => {
+    return (
+      <div className="card" key={card.index}>
+        <img src={card.image} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{card.title}</h5>
+          <p className="card-text">{card.text}</p>
+          <a href="www.google.com" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+    );
+  };
+
+  return <div className="container">{cardInfo.map(renderCard)}</div>;
 };
 
 export default Cardcomponent;

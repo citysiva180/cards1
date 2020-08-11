65bf88740813013ecae000958b3172da306246f9
Sivarajan <citysiva180@gmail.com>
2020-08-11 18:15:07 +0530

    added_navbar

--- src/components/cardComponents.jsx
+++ src/components/cardComponents.jsx
@@ -10,7 +10,7 @@ const Cardcomponent = () => {
   const cardInfo = [
     { image: sivapic, title: "Siva", text: "Fullstack Developer" },
     { image: rampic, title: "Ram", text: "Cyber security Engineer" },
-    { image: satzpic, title: "RockySatz", text: "Python Engineer" },
+    { image: satzpic, title: "RockySatz", text: "Data Scientist" },
     { image: dkpic, title: "Dk", text: "Fullstack Developer" },
   ];
 
@@ -29,7 +29,56 @@ const Cardcomponent = () => {
     );
   };
 
-  return <div className="container">{cardInfo.map(renderCard)}</div>;
+  return (
+    <React.Fragment>
+      <nav class="navbar navbar-expand-lg navbar-white bg-dark">
+        <a class="navbar-brand" href="#">
+          THE ROOMIES
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">
+                Home <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Features
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Pricing
+              </a>
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link disabled"
+                href="#"
+                tabindex="-1"
+                aria-disabled="true"
+              >
+                Disabled
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div className="container">{cardInfo.map(renderCard)}</div>
+    </React.Fragment>
+  );
 };
 
 export default Cardcomponent;
